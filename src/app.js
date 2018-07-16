04bbc0e8f696792124ec866b0569298c180c8ba4
pb <pijush.barik8@gmail.com>
2018-07-17 02:51:58 +0530

    updated

--- src/app.js
+++ src/app.js
@@ -1,7 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import KeysContainer from './KeysContainer'
-import Display from './Display'
+
+const KeysContainer = () => {
+  return <div id="keys-container">
+    <button className="drum-pads">Q</button>
+    <button className="drum-pads">W</button>
+    <button className="drum-pads">E</button>
+    <button className="drum-pads">A</button>
+    <button className="drum-pads">S</button>
+    <button className="drum-pads">D</button>
+    <button className="drum-pads">Z</button>
+    <button className="drum-pads">X</button>
+    <button className="drum-pads">C</button>
+  </div>
+};
+
+const Display = (props) => {
+  return <div id="display">{props.text}</div>
+};
 
 class DrumMachine extends React.Component {
   render() {
