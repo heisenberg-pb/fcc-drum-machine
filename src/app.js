1a873671ba939b4e4764de0682a5d4820c565dd9
pb <pijush.barik8@gmail.com>
2018-07-21 01:25:28 +0530

    Final commit

--- src/app.js
+++ src/app.js
@@ -1,75 +1,45 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-const Audio = () => {
-  return <div className="hidden">
-    <audio id="Q" className="clip">
-      <source src="https://github.com/heisenberg-pb/fcc-drum-machine/blob/master/audio_files/Heater-1.mp3?raw=true"></source>
-    </audio>
-
-    <audio id="W" className="clip">
-      <source src="https://github.com/heisenberg-pb/fcc-drum-machine/blob/master/audio_files/Heater-2.mp3?raw=true"></source>
-    </audio>
-
-    <audio id="E" className="clip">
-      <source src="https://github.com/heisenberg-pb/fcc-drum-machine/blob/master/audio_files/Heater-3.mp3?raw=true"></source>
-    </audio>
-
-    <audio id="A" className="clip">
-      <source src="https://github.com/heisenberg-pb/fcc-drum-machine/blob/master/audio_files/Heater-4_1.mp3?raw=true"></source>
-    </audio>
-
-    <audio id="S" className="clip">
-      <source src="https://github.com/heisenberg-pb/fcc-drum-machine/blob/master/audio_files/Heater-6.mp3?raw=true"></source>
-    </audio>
-
-    <audio id="D" className="clip">
-      <source src="https://github.com/heisenberg-pb/fcc-drum-machine/blob/master/audio_files/Dsc_Oh.mp3?raw=true"></source>
-    </audio>
-
-    <audio id="Z" className="clip">
-      <source src="https://github.com/heisenberg-pb/fcc-drum-machine/blob/master/audio_files/Kick_n_Hat.mp3?raw=true"></source>
-    </audio>
-
-    <audio id="X" className="clip">
-      <source src="https://github.com/heisenberg-pb/fcc-drum-machine/blob/master/audio_files/RP4_KICK_1.mp3?raw=true"></source>
-    </audio>
-
-    <audio id="C" className="clip">
-      <source src="https://github.com/heisenberg-pb/fcc-drum-machine/blob/master/audio_files/Cev_H2.mp3?raw=true"></source>
-    </audio>
-  </div>
-};
+import $ from 'jquery'
 
 const KeysContainer = (props) => {
   return <div id="keys-container">
-    <button className="drum-pads" 
+    <button className="drum-pad" 
             id="Heater-1"
-            onClick={props.keypresshandler}>Q</button>
-    <button className="drum-pads" 
+            onClick={props.onClick}>Q<audio id="Q" className="clip" src="https://github.com/heisenberg-pb/fcc-drum-machine/blob/master/audio_files/Heater-1.mp3?raw=true">
+            </audio></button>
+    <button className="drum-pad" 
             id="Heater-2"
-            onClick={props.keypresshandler}>W</button>
-    <button className="drum-pads" 
+            onClick={props.onClick}>W<audio id="W" className="clip" src="https://github.com/heisenberg-pb/fcc-drum-machine/blob/master/audio_files/Heater-2.mp3?raw=true">
+            </audio></button>
+    <button className="drum-pad" 
             id="Heater-3"
-            onClick={props.keypresshandler}>E</button>
-    <button className="drum-pads" 
+            onClick={props.onClick}>E<audio id="E" className="clip" src="https://github.com/heisenberg-pb/fcc-drum-machine/blob/master/audio_files/Heater-3.mp3?raw=true">
+            </audio></button>
+    <button className="drum-pad" 
             id="Heater-4"
-            onClick={props.keypresshandler}>A</button>
-    <button className="drum-pads" 
+            onClick={props.onClick}>A<audio id="A" className="clip" src="https://github.com/heisenberg-pb/fcc-drum-machine/blob/master/audio_files/Heater-4_1.mp3?raw=true">
+            </audio></button>
+    <button className="drum-pad" 
             id="Clap"
-            onClick={props.keypresshandler}>S</button>
-    <button className="drum-pads" 
+            onClick={props.onClick}>S<audio id="S" className="clip" src="https://github.com/heisenberg-pb/fcc-drum-machine/blob/master/audio_files/Heater-6.mp3?raw=true">
+            </audio></button>
+    <button className="drum-pad" 
             id="Open-HH"
-            onClick={props.keypresshandler}>D</button>
-    <button className="drum-pads" 
+            onClick={props.onClick}>D<audio id="D" className="clip" src="https://github.com/heisenberg-pb/fcc-drum-machine/blob/master/audio_files/Dsc_Oh.mp3?raw=true">
+            </audio></button>
+    <button className="drum-pad" 
             id="Kick-n-Hat"
-            onClick={props.keypresshandler}>Z</button>
-    <button className="drum-pads" 
+            onClick={props.onClick}>Z<audio id="Z" className="clip" src="https://github.com/heisenberg-pb/fcc-drum-machine/blob/master/audio_files/Kick_n_Hat.mp3?raw=true">
+            </audio></button>
+    <button className="drum-pad" 
             id="Kick"
-            onClick={props.keypresshandler}>X</button>
-    <button className="drum-pads" 
+            onClick={props.onClick}>X<audio id="X" className="clip" src="https://github.com/heisenberg-pb/fcc-drum-machine/blob/master/audio_files/RP4_KICK_1.mp3?raw=true">
+            </audio></button>
+    <button className="drum-pad" 
             id="Closed-HH"
-            onClick={props.keypresshandler}>C</button>
+            onClick={props.onClick}>C<audio id="C" className="clip" src="https://github.com/heisenberg-pb/fcc-drum-machine/blob/master/audio_files/Cev_H2.mp3?raw=true">
+            </audio></button>
   </div>
 };
 
@@ -84,21 +54,41 @@ class DrumMachine extends React.Component {
       display: "Welcome"
     };
     this.handleClick = this.handleClick.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+  }
+
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleKeyPress);
   }
 
   handleClick(e) {
-    var buttonClicked = document.getElementById(e.target.id);
-    var audioToBePlayed = document.getElementById(buttonClicked.innerHTML);
+    var buttonID = e.target.id;
+    var audioToBePlayed = e.target.childNodes[1];
     audioToBePlayed.play();
     this.setState({
-      display: e.target.id
+      display: buttonID
     });
   }
 
+  handleKeyPress(e) {
+    var keyPressed = e.key;
+    if(keyPressed != null) {
+      keyPressed = keyPressed.toUpperCase();
+    } else return;
+    if('QWEASDZXC'.indexOf(keyPressed) >= 0) {
+      var audioToBePlayed = document.getElementById(keyPressed);
+      audioToBePlayed.play();
+      var buttonContainsKeyPressed = $('button:contains('+keyPressed+')');
+      this.setState({
+        display: buttonContainsKeyPressed.attr('id')
+      });
+    }
+  }
+
   render() {
-    return <div id="drum-machine"> 
-      <Audio />
-      <KeysContainer keypresshandler={this.handleClick} /> 
+    return <div id="drum-machine"
+      onKeyPress={this.handleKeyPress}> 
+      <KeysContainer onClick={this.handleClick}/> 
       <Display text={this.state.display} />
     </div>
   }
